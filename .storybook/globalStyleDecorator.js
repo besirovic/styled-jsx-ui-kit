d5d0f9348424e45b7b95120a67162b23c1d10c72
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-06-02 04:20:06 +0200

    remove unused ThemeProvider

--- .storybook/globalStyleDecorator.js
+++ .storybook/globalStyleDecorator.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 export default (storyFn) => (
-  <ThemeProvider theme={baseTheme}>
-    <React.Fragment>
-      { storyFn() }
-      <style jsx global>{`
-        #root * {
-          font-family: Menlo,'Courier New',Courier,monospace;
-        }
-      `}</style>
-    </React.Fragment>
-  </ThemeProvider>
+  <React.Fragment>
+    { storyFn() }
+    <style jsx global>{`
+      #root * {
+        font-family: Menlo,'Courier New',Courier,monospace;
+      }
+    `}</style>
+  </React.Fragment>
 );
